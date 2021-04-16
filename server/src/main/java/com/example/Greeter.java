2f349f7617e468b49ffedbd808ead2be09535c2f
z2oce <82593367+z2oce@users.noreply.github.com>
2021-04-16 14:56:12 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -7,7 +7,10 @@ public class Greeter {
   public Greeter() {
 
   }
-
+/**
+* @param someone name of person
+* @return greeting string
+*/
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
